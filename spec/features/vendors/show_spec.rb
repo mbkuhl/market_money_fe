27d62ce99e12e2d6184c44935696e65684c7adc6
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-12-14 11:23:12 -0700

    Write tests for US 4

--- spec/features/vendors/show_spec.rb
+++ spec/features/vendors/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Vendors Show" do
 
   it 'will have a "more info" button next to each market that takes me to the market show page' do
     visit "/vendors/55823"
-save_and_open_page
+
     expect(page).to have_content("The Charcuterie Corner")
     expect(page).to have_content("Contact Info:")
     expect(page).to have_content("name: Claudie Langworth III")
@@ -13,13 +13,27 @@ save_and_open_page
     expect(page).to have_content("Description: Vendor selling a variety of artisanal cured meats and sausages.")
     
     visit "/vendors/55149"
-    save_and_open_page
     expect(page).to have_content("Leafy Greens")
     expect(page).to have_content("Contact Info:")
     expect(page).to have_content("name: Tari Ledner")
     expect(page).to have_content("phone: 1-129-873-5578")
     expect(page).to have_content("Credit Accepted?: YES")
     expect(page).to have_content("Description: sells a variety of leafy greens and salad mixes")
-    
+  end
+
+  it "can search for markets" do
+    visit "/vendors/55823"
+    expect(page).to have_content("Search for markets to Add")
+    expect(page).to_not have_content("Nob Hill Growers' Market")
+
+    fill_in :state, with: "New Mexico"
+    fill_in :city, with: "Albequerque"
+    fill_in :name, with: "Nob"
+
+    click_button "Search for Markets"
+    expect(current_path).to eq("/vendors/55823")
+
+    expect(page).to have_content("Nob Hill Growers' Market")
+    click_link "Nob Hill Growers' Market"
   end
 end
