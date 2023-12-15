be1c6899acd6ea4ba75f6866b9320a7732359dfd
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-12-15 08:44:05 -0700

    Refactor services to have one connection

--- app/services/market_service.rb
+++ app/services/market_service.rb
@@ -1,29 +1,21 @@
 class MarketService
 
-  def get_markets
-    response = markets_conn.get
-    JSON.parse(response.body, symbolize_names: true)
+  def market_conn
+    Faraday.new(url: "http://localhost:5000")
   end
 
-  def markets_conn
-    Faraday.new(url: "http://localhost:5000/api/v0/markets")
+  def get_markets
+    response = market_conn.get("/api/v0/markets")
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def get_market(id)
-    response = market_conn(id).get
+    response = market_conn.get("/api/v0/markets/#{id}")
     JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def market_conn(id)
-    Faraday.new(url: "http://localhost:5000/api/v0/markets/#{id}")
   end
 
   def search_markets(query)
-    response = search_markets_conn(query).get
+    response = market_conn.get("/api/v0/markets/search#{query}")
     JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def search_markets_conn(query)
-    Faraday.new(url: "http://localhost:5000/api/v0/markets/search#{query}")
   end
 end
